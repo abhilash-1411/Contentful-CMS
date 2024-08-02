5d374ffeca46ec5e78dfd2176f2d355eefea9b8d
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-08-02 19:02:52 +0530

    Fix all the changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,51 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const url=`${process.env.BASE_URL}/spaces/${process.env.SPACE_ID}/environments/master/entries?
-access_token=${process.env.ACCESS_TOKEN}`
-console.log(url)
+access_token=${process.env.ACCESS_TOKEN}&content_type=blog`
+
+
 export default async function Home() {
   const response = await fetch(url);
   const data = await response.json()
+  // console.log("Length:",data.items.length)
   return (
    <main>
     {
-     data.items.map((a:any,index:number)=>(
-      <div key={index}>
-        <h1 className="txt-3xl font-bold px-24">{a.fields.title}</h1>
-        <div className='py-4 px-24 pt-4'>{documentToReactComponents(a.fields.body)}</div>
-        {/* <Image/> */}
+     data.items.map((blog:any,index:number)=>{
+      const image = data.includes.Asset.find((asset:any)=>
+        asset.sys.id===blog.fields.image.sys.id
+    )
+    const authorEntry = data.includes.Entry.find((entry:any)=>
+        entry.sys.id===blog.fields.author.sys.id)
+
+    const authorImage = data.includes.Asset.find((auth:any)=>
+         auth.sys.id===authorEntry.fields.authorImage.sys.id
+    )
+      // console.log(image.fields.file.url);
+      // console.log("Author",authorEntry)
+      // console.log("Image",authorImage)
+
+      return(
+      <div key={index} className='px-4 mx-auto'>
+        <h1 className="text-3xl font-bold py-4">{blog.fields.title}</h1>
+        <div
+         className='py-4 '>{documentToReactComponents(blog.fields.body)}
+        </div>
+        
+        <img 
+        src={`https:`+image.fields.file.url} width={500} height={500} alt="blogimage"/>
+        <div className='flex gap-2 mt-10'>
+          <img src={authorImage.fields.file.url} width={500} height={500} alt="authorImage " className='h-16
+          w-16 rounded-full object-cover'/>
+              <h4>{authorEntry.fields.authorName}</h4>
+        </div>
+       
+        
       </div>
-     ))
-    }
+     )}
+    )}
+  
+   
    </main>
   );
 }
